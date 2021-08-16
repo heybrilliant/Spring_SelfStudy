1511f311a14af071cb5cb86ad915a67557eabf40
EunKyung Shin <heybrilliante@gmail.com>
2021-08-16 23:52:37 +0900

    210816 Spring review(memberpractice) - update carry's classes

---
+++ MemberPractice/src/main/java/com/carry/mp/carry/domain/Carry.java
@@ -0,0 +1,17 @@
+package com.carry.mp.carry.domain;
+
+import java.sql.Timestamp;
+
+public class Carry {
+
+	int idx;
+	String carryid;
+	String password;
+	String carryname;
+	String carrynick;
+	String carryphonenum;
+	String carryphoto;
+	Timestamp regdate;
+	
+	
+}
