bf12bfc2ed448c3b6193b63cd93d5a6d7675d6a3
EunKyung Shin <heybrilliante@gmail.com>
2021-09-12 23:55:08 +0900

    210912 FinalProject

--- MemberPractice/src/main/java/com/carry/mp/carry/controller/CarryJoinController.java
+++ MemberPractice/src/main/java/com/carry/mp/carry/controller/CarryJoinController.java
@@ -1,4 +1,4 @@
-package com.project.gymcarry.carry.controller;
+package com.carry.mp.carry.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
