83579bbe7a6e07d84a8c98ce48b18c0ca40ed568
EunKyung Shin <heybrilliante@gmail.com>
2021-10-07 23:35:50 +0900

    211007 DI주입 추가 설명

---
+++ ex00/src/main/java/org/zerock/sample/SampleHotel.java
@@ -0,0 +1,15 @@
+package org.zerock.sample;
+
+import org.springframework.stereotype.Component;
+
+// DI주입 복습 3가지 
+// : 1. 필드주입 / 2.세터주입/ 3.생성자주입
+@Component
+public class SampleHotel {
+
+	// Hotel에 Chef가 필요 ! -> 선언
+	private Chef chef;
+	// 이후 어떤 DI 주입을 이용할 것인지 판단
+	
+	}
+
