be36dafd7d453f88b00ed4b241661880750b4586
EunKyung Shin <heybrilliante@gmail.com>
2021-10-10 00:05:38 +0900

    211010 JDBC Tests

--- ex00/src/test/java/org/zerock/persistence/JDBCTests.java
+++ ex00/src/test/java/org/zerock/persistence/JDBCTests.java
@@ -1,5 +1,8 @@
 package org.zerock.persistence;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 import org.junit.Test;
 
 import lombok.extern.log4j.Log4j;
@@ -13,7 +16,14 @@ public class JDBCTests {
 		
 		Class clz = Class.forName("oracle.jdbc.driver.OracleDriver");
 		
-		log.info(clz);
+		// log.info(clz); -> 오라클 연결 확인용 로그 
+		
+		Connection con = DriverManager.getConnection(
+				"jdbc:oracle:thin:@localhost:8080:XE", "", "");
+		
+		log.info(con);
+		
+		con.close(); // bad code -> finally로 해줘야하는데 일단 log 확인용으로 만듬
 	}
 	
 }
